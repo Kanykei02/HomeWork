3e6123f9605c18e6df79189dd2cbd14d33451770
Kanykei02 <almazbekovakany@gmail.com>
2021-01-05 00:02:55 +0600

    Homework20

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Shop dress = new Shop(3000, 2);
+        Shop dress = new Shop(3000, 2);//ссори за позднюю здачу, я в целом поняла как это решается, но не до конца)
         Shop phone = new Shop(1000, 10);
         Shop notebook = new Shop(20000, 2);
 
