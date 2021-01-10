e8bc0ab35a4df187d2535768690834806f3c7e0e
Kanykei02 <almazbekovakany@gmail.com>
2021-01-10 23:56:09 +0600

    Homework24

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Bakable bread = new Bread();
+        bread.bake();
+        Drawable circle = new Circle();
+        circle.draw();
     }
 }
