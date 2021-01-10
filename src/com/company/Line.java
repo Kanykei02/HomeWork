cc3be456ae0528ff1428e8bc37accbf14c1776e8
Kanykei02 <almazbekovakany@gmail.com>
2021-01-10 17:22:00 +0600

    Homework22

---
+++ src/com/company/Line.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Line {
+     int x;
+     int y;
+
+     public Line(){}
+
+    public Line(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return getX() * getX();
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY2() {
+         return getX() * getX() * getX();
+    }
+}
---
+++ src/com/company/Painting.java
@@ -0,0 +1,29 @@
+package com.company;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class Painting extends Canvas {
+    public static final int FRAME_SIZE = 500;
+
+    @Override
+    public void paint(Graphics graphics) {
+        Line[] lines = new Line[7];
+        Random random = new Random();
+        for (int i = 0; i < lines.length; i++){
+            lines[i] = new Line(random.nextInt(50), random.nextInt(50));
+            graphics.setColor(Color.MAGENTA);
+            graphics.drawLine(lines[i].getX(), lines[i].getY(), lines[i].getX(), lines[i].getY2());
+            System.out.printf("X: %.2f, Y1: %.2f, Y2: %.2fn", lines[i].getX(), lines[i].getY(), lines[i].getY2());
+        }
+    }
+
+    public static void main(String[] args) {
+        Painting canvas = new Painting();
+        JFrame jframe = new JFrame();
+        jframe.setSize(FRAME_SIZE,FRAME_SIZE);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.getContentPane().add(canvas);
+        jframe.setVisible(true);
+    }
+}
