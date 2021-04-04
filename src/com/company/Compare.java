6138daa30dbf83ca1ce339f967a43cfb0574b165
Kanykei02 <almazbekovakany@gmail.com>
2021-04-04 20:06:21 +0600

    hw45

---
+++ src/com/company/Compare.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Comparator;
+
+public class Compare implements Comparator<City> {
+    @Override
+    public int compare(City o1, City o2) {
+        if (o1.getCode() == o2.getCode()) return 0;
+        else if (o1.getCode() < o2.getCode()) return 1;
+        return -1;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Random ran = new Random();
+        ArrayList<City> arrCity = new ArrayList<>();
+        String[] cities = {"Hamburg", "Berlin", "München", "Köln", "Hessen", "Sachsen", "Thüringen", "Saarland", "Bremen", "Nordrhein-Westfalen"};
+
+        for (int i = 0; i < 10; i++){
+            arrCity.add(new City(cities[i], ran.nextInt((500 - 50) + 1) + 50));
+        }
+        System.out.println(arrCity);
+
+        Set<City> city2 = new HashSet<>();
+        Set<City> city3 = new TreeSet<>();
+
+        for (City c: arrCity){
+            if (c.getCode() % 2 == 0) city2.add(c);
+           // else city3.add(c); почему-то если я его активирую, то программа рушится...
+        }
+        System.out.println(city2);
+        System.out.println(city3);
+
+        city3.removeIf(city -> city.getName().length() > 5);
+        Set<City> city4 = new TreeSet<>(new Compare());
+        city4.addAll(city3);
     }
 }
